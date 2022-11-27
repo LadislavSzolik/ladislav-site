875f32c8d0c308f026d2c34195b5641f53bc71bf
Ladislav Szolik <szolik.ladislav@gmail.com>
2022-11-27 15:15:21 +0100

    Remove remote font

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,26 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy({"src/assets/images": "images"});
-    eleventyConfig.addPassthroughCopy({"src/assets/downloads": "downloads"});
-    eleventyConfig.addPassthroughCopy({"src/assets/favicon.ico": "favicon.ico"});
-    eleventyConfig.addPassthroughCopy({"src/assets/robots.txt": "robots.txt"});
+  eleventyConfig.addPassthroughCopy({ "src/assets/images": "images" });
+  eleventyConfig.addPassthroughCopy({ "src/assets/downloads": "downloads" });
+  eleventyConfig.addPassthroughCopy("src/assets/fonts");
+  eleventyConfig.addPassthroughCopy({ "src/assets/favicon.ico": "favicon.ico" });
+  eleventyConfig.addPassthroughCopy({ "src/assets/robots.txt": "robots.txt" });
 
-    eleventyConfig.addPairedNunjucksShortcode("lessonsLearned", (text) => `
+  eleventyConfig.addPairedNunjucksShortcode(
+    "lessonsLearned",
+    (text) => `
         <div class="max-w-3xl mx-auto text-center bg-sky-100 rounded-2xl px-4 sm:px-7 py-4">
             <h3 class="font-semibold">Lessons learned</h3>
             <p>${text}</p>
         </div>
-  `)
+  `
+  );
 
-    return {
-        dir: {
-            input: "src",
-            data: "_data",
-            includes: "_includes",
-            layouts: "_layouts",
-        },
-    };
+  return {
+    dir: {
+      input: "src",
+      data: "_data",
+      includes: "_includes",
+      layouts: "_layouts",
+    },
+  };
 };
